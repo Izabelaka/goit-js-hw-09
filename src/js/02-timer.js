cde264ec140b2f222c76094abf3779a55513d3e1
Izabela Kazana <izabelaxkazana@gmail.com>
2023-05-12 11:37:41 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,66 @@
+// const timerFields = {
+//   days: document.querySelector('[data-days]'),
+//   hours: document.querySelector('[data-hours]'),
+//   minutes: document.querySelector('[data-minutes]'),
+//   seconds: document.querySelector('[data-seconds]'),
+// };
+
+// const startButton = document.querySelector('[data-start]');
+// startButton.disabled = true;
+// let countdownInterval = null;
+// let selectedDate = null;
+
+// flatpickr('#datetime-picker', {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     selectedDate = selectedDates[0];
+//     if (selectedDate <= new Date()) {
+//       alert('Please choose a date in the future');
+//       startButton.disabled = true;
+//     } else {
+//       startButton.disabled = false;
+//     }
+//   },
+// });
+
+// startButton.addEventListener('click', () => {
+//   startButton.disabled = true;
+//   countdownInterval = setInterval(() => {
+//     const timeLeft = selectedDate - new Date();
+//     if (timeLeft <= 0) {
+//       clearInterval(countdownInterval);
+//       alert('Time is up!');
+//     } else if (timeLeft < 1000) {
+//       clearInterval(countdownInterval);
+//       alert('Time is up!');
+//     } else {
+//       const time = convertMs(timeLeft);
+//       timerFields.days.textContent = addLeadingZero(time.days);
+//       timerFields.hours.textContent = addLeadingZero(time.hours);
+//       timerFields.minutes.textContent = addLeadingZero(time.minutes);
+//       timerFields.seconds.textContent = addLeadingZero(time.seconds);
+//     }
+//   }, 1000);
+// });
+
+// function convertMs(ms) {
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+//   const days = Math.floor(ms / day);
+//   const hours = Math.floor((ms % day) / hour);
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+//   return { days, hours, minutes, seconds };
+// }
+
+// function addLeadingZero(value) {
+//   return String(value).padStart(2, '0');
+// }
 const timerFields = {
   days: document.querySelector('[data-days]'),
   hours: document.querySelector('[data-hours]'),
@@ -10,18 +73,25 @@ startButton.disabled = true;
 let countdownInterval = null;
 let selectedDate = null;
 
-flatpickr('#datetime-picker', {
+let flatpickrInstance = flatpickr('#datetime-picker', {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    selectedDate = selectedDates[0];
-    if (selectedDate <= new Date()) {
-      alert('Please choose a date in the future');
-      startButton.disabled = true;
-    } else {
-      startButton.disabled = false;
+    if (selectedDates.length > 0) {
+      const newSelectedDate = selectedDates[0];
+      if (newSelectedDate <= new Date()) {
+        alert('Please choose a date in the future');
+      } else {
+        // Usunięcie poprzedniego odliczania
+        clearInterval(countdownInterval);
+        // Wyzerowanie licznika intervala
+        countdownInterval = null;
+        // Wybranie nowej daty
+        selectedDate = newSelectedDate;
+        startButton.disabled = false;
+      }
     }
   },
 });
@@ -32,6 +102,7 @@ startButton.addEventListener('click', () => {
     const timeLeft = selectedDate - new Date();
     if (timeLeft <= 0) {
       clearInterval(countdownInterval);
+      countdownInterval = null;
       alert('Time is up!');
     } else {
       const time = convertMs(timeLeft);
